5c8deba292cc3b498946f5321d21ca32f07f3598
Dmytry Bezryk <espado1512b@gmail.com>
2021-05-06 15:40:34 +0300

    four commit for master

---
+++ master3.py
@@ -0,0 +1,4 @@
+from . import master2
+
+if __name__ == '__main__':
+	print(master2.plus(1, 2))
--- test_mod_for_commit2.py
+++
@@ -1,4 +0,0 @@
-from . import test_mod_for_commit
-
-if __name__ == '__main__':
-	print(test_mod_for_commit.plus(1, 2))
